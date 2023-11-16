53db5a426adb714ccd672785d04b9ce673fb4cd8
young2866 <62212610+young2866@users.noreply.github.com>
2023-11-16 17:26:43 +0900

    DML practice

--- basic/2-P.DML.sql
+++ basic/2-P.DML.sql
@@ -7,7 +7,7 @@ values(4, 'choi', 'abc3@naver.com', '1234', 'User');
 INSERT INTO author(id, name, email, password, role) 
 values(5, 'jung', 'abc4@naver.com', '1234', 'User');
 INSERT INTO author(id, name, email, password, role) 
-values(5, 'hwang', 'abc5@naver.com', '1234', 'User');
+values(6, 'hwang', 'abc5@naver.com', '1234', 'User');
 
 INSERT INTO post(id, title, contents, author_id) 
 values(1, 'hello1', 'go to hello', 1);
@@ -26,3 +26,15 @@ values(5, 'hello55', 'go to hello', 55);
 SQL Error [1452] [23000]: (conn=13) Cannot add or update a child row
 : a foreign key constraint fails (`board`.`post`,
  CONSTRAINT `post_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`))
+
+-- author 데이터 중 id가 4인 데이터의 email을 abc@naver.com, name을 abc로 변경
+UPDATE author SET email='abc@naver.com', name='abc' WHERE id=4;
+-- post에 글을 작성 하지 않은 author 데이터 1개 삭제
+DELETE FROM author WHERE id=5;
+-- post에 글 쓴적이 있는 author 데이터 1개 삭제 -> 에러 -> 조치 후 삭제
+DELETE FROM author WHERE id=1;
+ERROR 1451 (23000): Cannot delete or update a parent row
+: a foreign key constraint fails (`board`.`post`,
+ CONSTRAINT `post_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`))
+DELETE FROM post WHERE author_id = 1;
+DELETE FROM author WHERE id=1;
--- basic/2.DML.sql
+++ basic/2.DML.sql
@@ -4,3 +4,7 @@
 INSERT INTO author(id, name, email) values(1, 'kim', 'abc@naver.com');
 INSERT INTO author(id, name, email, password, role) 
 values(4, 'choi', 'abc3@naver.com', '1234', 'User');
+-- author 테이블의 id값이 1인 데이터의 email 주소를 수정
+UPDATE author SET email='abc123@test.com' WHERE id=1;
+-- author_id가 5인 author 테이블의 데이터 값을 지운다
+DELETE FROM author WHERE author_id=5;
