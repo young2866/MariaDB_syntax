f47bfc3b2047bd427db5be0dba8e95be33286bb3
young2866 <62212610+young2866@users.noreply.github.com>
2023-11-20 12:21:40 +0900

    unique constraint

--- basic/2-P.DML.sql
+++ basic/2-P.DML.sql
@@ -38,3 +38,39 @@ ERROR 1451 (23000): Cannot delete or update a parent row
  CONSTRAINT `post_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`))
 DELETE FROM post WHERE author_id = 1;
 DELETE FROM author WHERE id=1;
+
+-- post의 title 칼럼의 제약조건을 not null로 변경
+ALTER TABLE post MODIFY COLUMN title varchar(255) not null;
+
+--post 테이블의 id에 auto_increment적용
+alter table post MODIFY COLUMN id INT AUTO_INCREMENT;
+
+--post 테이블에 id 없이 insert
+INSERT INTO post (title, contents, author_id) VALUES ("1234", "1536", 2);
+
+--위의 데이터 삭제
+DELETE FROM post WHERE id = 6;
+
+-- 다시 테이터 생성
+-- auto_increment로 id값이 1 증가된 7인것을 볼 수 있다.
+INSERT INTO post (title, contents, author_id) VALUES ("1234", "1536", 2);
+
+--now()를 통해서 오늘 생성된 데이터 출력
+SELECT * from post 
+WHERE DATE_FORMAT(created_time,'%Y-%m-%d') = DATE_FORMAT(now(), '%Y-%m-%d');
+
+
+-- 테이블의 인덱스 조회
+SHOW INDEX FROM 테이블명
+-- 테이블의 인덱스를 삭제
+ALTER TABLE 테이블이름 DROP INDEX 인덱스명;
+
+-- author 테이블의 email 컬럼에 unique제약 조건 추가
+ALTER TABLE author MODIFY COLUMN email VARCHAR(255) UNIQUE;
+--제약조건 조회
+SELECT * FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE TABLE_NAME = 'author';
+-- email 컬럼의 제약조건 삭제
+ALTER TABLE author DROP CONSTRAINT email_unique;
+-- author 테이블의 email 컬럼에 unique제약 조건 추가(방법 2)
+ALTER TABLE author ADD CONSTRAINT unique_email UNIQUE(email);
--- basic/2.DML.sql
+++ basic/2.DML.sql
@@ -8,3 +8,11 @@ values(4, 'choi', 'abc3@naver.com', '1234', 'User');
 UPDATE author SET email='abc123@test.com' WHERE id=1;
 -- author_id가 5인 author 테이블의 데이터 값을 지운다
 DELETE FROM author WHERE author_id=5;
+-- SELECT
+SELECT 필드이름 FROM 테이플이름 [WHERE 조건]
+-- author 테이블의 모든 것 조회
+select * from author;
+-- author 테이블의 name만 조회
+select name form author;
+-- author 테이블의 id가 1인 데이터 조회
+select * from author WHERE id=1;
